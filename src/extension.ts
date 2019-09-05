eea94b72d2cb5210af2fb074f3189c36eb83efc0
So Fujinami <s-fujinami@saci.kyoto-u.ac.jp>
2019-09-05 14:42:04 +0900

    refactor: separate provider code specific to built-in symbols

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {SpecBuiltinProvider as provider} from './specProvider';
+import {SpecBuiltinProvider as BuiltinProvider} from './specBuiltinProvider';
 
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-	const builtinProvider = new provider(context.asAbsolutePath('./syntaxes/spec.apiReference.json'));
+	const builtinProvider = new BuiltinProvider(context.asAbsolutePath('./syntaxes/spec.apiReference.json'));
 	const selector = { scheme: '*', language: 'spec' };
 	context.subscriptions.push(
 		vscode.languages.registerCompletionItemProvider(selector, builtinProvider),
